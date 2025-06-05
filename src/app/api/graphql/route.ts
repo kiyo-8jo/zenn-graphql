0fe31be99b52e97cb3342e88e9f1be8213d43515
kiyo-8jo <csta13054@gmail.com>
2025-06-05 12:04:23 +0900

    test

---
+++ src/app/api/graphql/route.ts
@@ -0,0 +1,25 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { ApolloServer } from "@apollo/server";
+import { Resolvers } from "../../../../apollo/__generated__/server/resolvers-types";
+
+const typeDefs = readFileSync(
+  join(process.cwd(), "apollo/documents/schema.gql"),
+  "utf-8"
+);
+
+const resolvers: Resolvers = {
+  Query: {
+    users() {
+      return [{ name: "Nextjs" }, { name: "Nuxtjs" }, { name: "Sveltekit" }];
+    },
+  },
+};
+
+const apolloServer = new ApolloServer<Resolvers>({ typeDefs, resolvers });
+
+const handler =  startServerAndCreateNextHandler(apolloServer);
+
+export {handler as GET, handler as POST}
+
